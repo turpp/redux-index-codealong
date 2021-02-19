b03b961bc4c70a2f0af08b51ef54bac6542f54e8
turpp <white.tripp@icloud.com>
2021-02-19 17:37:33 -0600

    Done.

---
+++ src/components/todos/TodosContainer.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import Todo from './Todo'
+
+class TodosContainer extends Component{
+
+    renderTodos = () => {
+        return this.props.todos.map((todo, id) =>{
+            // debugger
+            return <Todo key={id} text={todo} />
+        })
+    }
+
+    render(){
+        console.log(this.props.todos)
+        return(
+            <div>
+                {this.renderTodos()}
+            </div>
+        )
+    }
+}
+const mapStateToProps = (state) =>{
+return {todos: state.todos}
+}
+export default connect(mapStateToProps)(TodosContainer)
